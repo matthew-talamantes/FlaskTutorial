4011ec664a4a307628ef4b687b8e146e5851daad
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-07-31 10:25:37 -0400

    Implement and test delete method

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import Flask, request
 from flask_restful import Api, Resource, reqparse, abort
 
@@ -12,9 +13,13 @@ video_put_args.add_argument('views', type=int, help='The views of the video is r
 
 videos = {}
 
-def video_exists(video_id):
+def video_doesnt_exists(video_id):
     if video_id not in videos.keys():
         abort(404, message='Video not found...')
+
+def video_exists(video_id):
+    if video_id in videos.keys():
+        abort(409, message='Video already exists with that ID...')
 
 class HelloWorld(Resource):
     def get(self, name):
@@ -25,12 +30,19 @@ class HelloWorld(Resource):
 
 class Video(Resource):
     def get(self, video_id):
-        video_exists(video_id)
+        video_doesnt_exists(video_id)
         return videos[video_id]
+
     def put(self, video_id):
+        video_exists(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+    
+    def delete(self, video_id):
+        video_doesnt_exists(video_id)
+        del videos[video_id]
+        return '', 204
 
 api.add_resource(HelloWorld, "/helloworld/<string:name>")
 api.add_resource(Video, '/video/<int:video_id>')
--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
-response = requests.put(BASE + "video/1", {'likes': 10, 'name': 'test video', 'views': 101})
-print(response.json())
-video_id = input('Enter video Id: ')
+data = [{'likes': 10, 'name': 'test video', 'views': 101}, {'likes': 35, 'name': 'test video2', 'views': 155}, {'likes': 80, 'name': 'test video3', 'views': 3000}]
+for count, video in enumerate(data):
+    response = requests.put(BASE + f"video/{count + 1}", video)
+    print(response.json())
+video_id = input('Enter video Id to view: ')
 response = requests.get(BASE + f"video/{video_id}")
 print(response.json())
+video_id = input('Enter video ID to delete: ')
+response = requests.delete(BASE + f"video/{video_id}")
+print(response.status_code)
